f14c7c1042af933514b85919acbbc9186a374c3e
Gerson Abreu <125635864+GersonAbreu17@users.noreply.github.com>
2024-06-15 01:50:47 -0300

    Add files via upload

--- IHM/script.js
+++ IHM/script.js
@@ -1,9 +1,9 @@
 
+var cabecalho = document.getElementsByTagName('nav');
 
 window.onscroll = function() { fixarCabecalho() };
 
 function fixarCabecalho() {
-    var cabecalho = document.getElementsByTagName('nav');
     // Adicionando ou removendo sombra dependendo da posição do scroll
     if (window.scrollY <= 75) {
         cabecalho[0].classList.remove("com-sombra");
@@ -11,3 +11,23 @@ function fixarCabecalho() {
         cabecalho[0].classList.add("com-sombra");
     }
 }
+
+
+
+// Obtém a posição vertical do rodapé em relação ao topo da página
+
+// Adiciona um listener de scroll
+window.addEventListener('scroll', function() {
+    
+    var nav = document.querySelector('nav');
+    var footer = document.querySelector('footer');
+    
+    var footerPosition = footer.offsetTop;
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition >= footerPosition) {
+        nav.style.backgroundColor = '#1e2021';
+    } else {
+        nav.style.backgroundColor = ''; 
+    }
+});
